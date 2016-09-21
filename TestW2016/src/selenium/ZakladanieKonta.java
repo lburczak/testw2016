0e8a2885f005f82bd78fee59524daa33ad7ea0c2
lburczak <lukasz.burczak@forprogress.com.pl>
2016-09-21 14:54:48 +0200

    Szkielet projektu: selenium

---
+++ TestW2016/src/selenium/ZakladanieKonta.java
@@ -0,0 +1,19 @@
+package selenium;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class ZakladanieKonta {
+  @Test
+  public void Zakladanie() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
