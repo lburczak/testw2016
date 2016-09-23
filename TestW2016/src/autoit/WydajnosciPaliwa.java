cf53923a21a090d0072cb8d86bc22cc9e652c122
lburczak <lukasz.burczak@forprogress.com.pl>
2016-09-23 23:38:58 +0200

    Test selenium - Zakladanie konta

--- TestW2016/src/autoit/WydajnosciPaliwa.java
+++ TestW2016/src/autoit/WydajnosciPaliwa.java
@@ -14,30 +14,53 @@ import com.jacob.com.LibraryLoader;
 public class WydajnosciPaliwa {
 	public AutoItX autoit=null;
 	public String actualTitle = null;
+		
+	@BeforeTest
+	public void beforeTest() {
+		String jacobDllVersionToUse;
+
+		if (jvmBitVersion().contains("32")) {
+			jacobDllVersionToUse = "jacob-1.18-M2-x86.dll";
+		} else {
+			jacobDllVersionToUse = "jacob-1.18-M2-x64.dll";
+		}
+		File file = new File("C:\\Program Files\\ForProgress\\ForEVO\\drivers\\", jacobDllVersionToUse);
+		
+		System.out.println(jacobDllVersionToUse);
+		System.setProperty(LibraryLoader.JACOB_DLL_PATH, file.getAbsolutePath());
+
+		autoit = new AutoItX();
+		autoit.run("calc.exe");
+		actualTitle = autoit.winGetTitle("[ACTIVE]");
+		autoit.winActivate(actualTitle);
+		autoit.winWaitActive(actualTitle);
+	}
+
+	@AfterTest
+	public void afterTest() {
+		autoit.winClose(actualTitle);
+	}
+	
 	@Test(priority=0)
 	public void sprawdzTytul() {
-		actualTitle = autoit.winGetTitle("[ACTIVE]");
+		
 		Assert.assertEquals(actualTitle, "Kalkulator");
 	}
 	
 	@Test(priority=1)
 	public void uruchomWydajnoscPaliwa() throws InterruptedException {
 		autoit.winActivate(actualTitle);
-		autoit.send("!{W}",false);
+		
+		// Naciœniêcie przycisku ALT+W
+		autoit.send("!{W}",false);		
 		Thread.sleep(1000);
+		
+		// Naciœniêcie przycisku A przy trzymanym ALT
 		autoit.send("{A}",false);
 		Thread.sleep(1000);
-		autoit.send("{A}",false);
 		
-	}
-	
-	@Test(priority=4)
-	public void uruchomWidokPodstawowy() throws InterruptedException {
-		autoit.winActivate(actualTitle);
-		
-		autoit.send("!{W}",false);
-		Thread.sleep(1000);
-		autoit.send("{P}",false);		
+		// Naciœniêcie przycisku A przy trzymanym ALT
+		autoit.send("{A}",false);		
 	}
 	
 	@Test(priority=2)
@@ -55,30 +78,14 @@ public class WydajnosciPaliwa {
 		String wynik = autoit.controlGetText(actualTitle, "", "[CLASS:Edit; INSTANCE:7]");
 		Assert.assertEquals(wynik, "5");
 	}
-
-	@BeforeTest
-	public void beforeTest() {
-		String jacobDllVersionToUse;
-
-		if (jvmBitVersion().contains("32")) {
-			jacobDllVersionToUse = "jacob-1.18-M2-x86.dll";
-		} else {
-			jacobDllVersionToUse = "jacob-1.18-M2-x64.dll";
-		}
-		File file = new File("C:\\Program Files\\ForProgress\\ForEVO\\drivers\\", jacobDllVersionToUse);
-		
-		System.out.println(jacobDllVersionToUse);
-		System.setProperty(LibraryLoader.JACOB_DLL_PATH, file.getAbsolutePath());
-
-		autoit = new AutoItX();
-		autoit.run("calc.exe");
+	
+	@Test(priority=4)
+	public void uruchomWidokPodstawowy() throws InterruptedException {
 		autoit.winActivate(actualTitle);
-		autoit.winWaitActive(actualTitle);
-	}
-
-	@AfterTest
-	public void afterTest() {
-		autoit.winClose(actualTitle);
+		
+		autoit.send("!{W}",false);
+		Thread.sleep(1000);
+		autoit.send("{P}",false);		
 	}
 
 	/**
--- TestW2016/src/selenium/ZakladanieKonta.java
+++ TestW2016/src/selenium/ZakladanieKonta.java
@@ -2,18 +2,112 @@ package selenium;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 
 public class ZakladanieKonta {
-  @Test
-  public void Zakladanie() {
-  }
-  @BeforeTest
-  public void beforeTest() {
-  }
+	public WebDriver browser = null;
+	public String url = "http://127.0.0.1:8080/gardenstore/pl/";
+	public String user = "test226@test.pl";
+	public String pass = "qwert1234";
 
-  @AfterTest
-  public void afterTest() {
-  }
+	@BeforeTest
+	public void beforeTest() {
+		browser = new ChromeDriver();
+		browser.get(url);
+		browser.manage().window().maximize();
+		browser.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+	@AfterTest
+	public void afterTest() {
+		 browser.quit();
+	}
 
+	@Test(priority = 0)
+	public void otwarcieStronyLogowania() {
+		WebElement btnLogin = browser.findElement(By.xpath("//*[@id='header']/div[2]/div/div/nav/div[1]/a"));
+		btnLogin.click();
+
+		Assert.assertEquals(browser.getTitle(), "Logowanie - GARDEN STORE");
+	}
+
+	@Test(priority = 1)
+	public void zalozenieKonta() throws InterruptedException {
+		WebElement editCreateMail = browser.findElement(By.id("email_create"));
+		WebElement btnCreateAcc = browser.findElement(By.id("SubmitCreate"));
+
+		editCreateMail.sendKeys(user);
+		btnCreateAcc.click();
+
+		WebElement rbMale = browser.findElement(By.id("id_gender1"));
+		WebElement editFirstName = browser.findElement(By.id("customer_firstname"));
+		WebElement editLastName = browser.findElement(By.id("customer_lastname"));
+		WebElement editPswd = browser.findElement(By.id("passwd"));
+		WebElement selectDays = browser.findElement(By.id("days"));
+		WebElement selectMonths = browser.findElement(By.id("months"));
+		WebElement selectYears = browser.findElement(By.id("years"));
+		WebElement chbNews = browser.findElement(By.id("newsletter"));
+		WebElement chbOptin = browser.findElement(By.id("optin"));
+		WebElement btnSubmit = browser.findElement(By.id("submitAccount"));
+
+		rbMale.click();
+		editFirstName.sendKeys("FirstName");
+		editLastName.sendKeys("LastName");
+		editPswd.sendKeys(pass);
+
+		// praca z comboBoxami
+		Select select = new Select(selectDays);
+		
+		select.selectByValue("10");
+		select = new Select(selectMonths);
+		
+		select.selectByVisibleText("Czerwiec ");
+		select = new Select(selectYears);	
+		select.selectByValue("1983");
+
+		chbNews.click();
+		chbOptin.click();
+
+		btnSubmit.click();
+
+		// czekamy na pojawienie siê tekstu - konto stworzone domyœlnie
+		new WebDriverWait(browser, 5)
+				.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='center_column']/p[1]")));
+		
+	
+	}
+
+	@Test(priority = 2)
+	public void wylogujSie() {
+		WebElement btnLogOut = browser.findElement(By.xpath("//*[@id='header']/div[2]/div/div/nav/div[2]/a"));
+		btnLogOut.click();
+	}
+
+	@Test(priority = 3)
+	public void zalogujSie() {
+
+		if (!browser.getTitle().equals("Logowanie - GARDEN STORE")) {
+			WebElement btnLogin = browser.findElement(By.xpath("//*[@id='header']/div[2]/div/div/nav/div[1]/a"));
+			btnLogin.click();
+		}
+
+		WebElement editEmail = browser.findElement(By.id("email"));
+		WebElement editPswd = browser.findElement(By.id("passwd"));
+		WebElement btnSubmit = browser.findElement(By.id("SubmitLogin"));
+
+		editEmail.sendKeys(user);
+		editPswd.sendKeys(pass);
+		btnSubmit.click();
+		Assert.assertEquals(browser.getTitle(), "Moje konto - GARDEN STORE");
+	}
 }
